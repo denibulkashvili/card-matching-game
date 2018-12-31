bafa340d39b6238ea4800994b2f8417a0a6a7d45
denibulkashvili <denibulkashvili@gmail.com>
2018-12-31 11:02:40 +0700

    Set up enzyme and write a first test for App component

--- card-matching-game/src/App.js
+++ card-matching-game/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className='app-title'>Card Matching Game</h1> 
         <DisplayGrid />
         <Button />
       </div>
--- card-matching-game/src/App.test.js
+++ card-matching-game/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import './tests/setup';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('shows title correctly', () => {
+    const component = shallow(<App />);
+    expect(component.find('.app-title').text()).toMatch(/Card Matching Game/);
+  });
 });
+
