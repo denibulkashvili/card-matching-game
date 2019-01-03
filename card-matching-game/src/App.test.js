1e960cebc93dfb4df3a6db9b0ad05bdf334582eb
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 06:14:45 +0700

    Test that gameState default is stopped

--- card-matching-game/src/App.test.js
+++ card-matching-game/src/App.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import App from './App';
 import './tests/setup';
 import { shallow } from 'enzyme';
+
+import App from './App';
 
 describe('<App />', () => {
   it('shows title correctly', () => {
     const component = shallow(<App />);
     expect(component.find('.app-title').text()).toMatch(/Card Matching Game/);
   });
+
+  it('starts with the gameState equal Stopped', () => {
+    const component = shallow(<App />);
+    const appState = component.state().gameState
+    expect(appState).toEqual(0);
+  })
+  
 });
 
