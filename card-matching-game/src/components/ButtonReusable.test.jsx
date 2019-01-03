65fdcc0a54303549572a0f85b89a73e033803f72
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 06:15:14 +0700

    Add initial button component test cases

---
+++ card-matching-game/src/components/ButtonReusable.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../tests/setup';
+import { shallow, mount } from 'enzyme';
+import Display from './DisplayGrid';
+import App from '../App';
+import Button from './ButtonReusable';
+
+describe('<Button />', () => {
+  it('displays a button labeled Start', () => {
+    const component = shallow(<Button buttonLabel="Start"/>);
+    const text = component.find('.btn').text();
+    expect(text).toEqual('Start');
+  })
+
+  it('simulates click event', () => {
+    const appComponent = mount(<App />);
+    const button = appComponent.find('.btn');
+    button.simulate('click');
+    const gameStateDisplay = appComponent.find('div#display').text();
+    expect(gameStateDisplay).toMatch(/This is a display grid. The game is now Running./);
+  })
+});
+
