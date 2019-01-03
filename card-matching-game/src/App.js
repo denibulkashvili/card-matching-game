886716b0874d2d0f9ef03acd90990ff53a764f3d
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 06:11:54 +0700

    Add gameStates and a method to get game state name

--- card-matching-game/src/App.js
+++ card-matching-game/src/App.js
@@ -3,12 +3,31 @@ import Button from './components/ButtonReusable';
 import DisplayGrid from './components/DisplayGrid';
 
 
+const gameState = {
+  Stopped: 0,
+  Running: 1,
+  Finished: 2
+}
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       gameState: gameState.Stopped,
     };
+  }
+
+  get gameStateName() {
+    const currentGameState = this.state.gameState;
+    switch(currentGameState) {
+      case 0:
+        return 'Stopped';
+      case 1:
+        return 'Running';
+      case 2:
+        return 'Finished';
+      default:
+        return '';
+    }
   }
   render() {
     return (
