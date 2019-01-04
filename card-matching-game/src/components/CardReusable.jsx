cefc536f2190e1a41e4347f1d9075134d84f7334
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 13:15:34 +0700

    Pass props to Card components

--- card-matching-game/src/components/CardReusable.jsx
+++ card-matching-game/src/components/CardReusable.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div>
-      Card image/text.
-    </div>
+    <img 
+      className="image" 
+      src={props.src} 
+      alt=""
+    />
   )
 }
--- card-matching-game/src/components/DisplayGrid.jsx
+++ card-matching-game/src/components/DisplayGrid.jsx
@@ -3,11 +3,21 @@ import Card from './CardReusable';
 
 export default function DisplayGrid(props) {
 
-    return (
-      <div id="display">
-        This is a display grid. The game is now {this.props.gameStateName}.
-        <Card />
-        <Card />
-      </div>
-    )
-  }
+  return (
+    <div id="display">
+      This is a display grid. The game is now {props.gameStateName}.
+      <Card src={"/img/img01.jpg"} vocabId={1} />
+      <Card src={"/img/img02.jpg"} vocabId={2} />
+      <Card src={"/img/img03.jpg"} vocabId={3} />
+      <Card src={"/img/img04.jpg"} vocabId={4} />
+      <Card src={"/img/img05.jpg"} vocabId={5} />
+      <Card src={"/img/img06.jpg"} vocabId={6} />
+      <Card src={"/img/txt01.jpg"} vocabId={1} />
+      <Card src={"/img/txt02.jpg"} vocabId={2} />
+      <Card src={"/img/txt03.jpg"} vocabId={3} />
+      <Card src={"/img/txt04.jpg"} vocabId={4} />
+      <Card src={"/img/txt05.jpg"} vocabId={5} />
+      <Card src={"/img/txt06.jpg"} vocabId={6} />
+    </div>
+  )
+}
