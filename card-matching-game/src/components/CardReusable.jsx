9a6467c3164faea1ab3e4365c6f4cda7f18635b2
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 15:38:30 +0700

    Add function to flip the image on click

--- card-matching-game/src/components/CardReusable.jsx
+++ card-matching-game/src/components/CardReusable.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default function Card(props) {
-  return (
-    <img 
-      className="image" 
-      src={props.src} 
-      alt=""
-    />
-  )
+export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCardUncovered: false,
+      cardState: 'hidden'
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isCardUncovered: !prevState.isCardUncovered
+    }));
+    console.log(`Card is now ${this.state.isCardUncovered ? 'uncovered' : 'covered'}`);    
+  }
+  
+  render() {
+    return (
+      <img 
+        className="image" 
+        src={this.state.isCardUncovered ? this.props.src : "/img/cover.png"} 
+        alt=""
+        onClick={this.handleClick}
+      />
+    )
+  }
+  
 }
