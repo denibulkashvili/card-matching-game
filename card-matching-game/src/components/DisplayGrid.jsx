d20456268bf876457c3a8633a0bd06a97316ad52
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 16:07:36 +0700

    Update display grid

--- card-matching-game/src/components/DisplayGrid.jsx
+++ card-matching-game/src/components/DisplayGrid.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './CardReusable';
-
-export default function DisplayGrid(props) {
-
-  return (
-    <div id="display">
-      This is a display grid. The game is now {props.gameStateName}.
-      <Card src={"/img/img01.jpg"} vocabId={1} />
-      <Card src={"/img/img02.jpg"} vocabId={2} />
-      <Card src={"/img/img03.jpg"} vocabId={3} />
-      <Card src={"/img/img04.jpg"} vocabId={4} />
-      <Card src={"/img/img05.jpg"} vocabId={5} />
-      <Card src={"/img/img06.jpg"} vocabId={6} />
-      <Card src={"/img/txt01.jpg"} vocabId={1} />
-      <Card src={"/img/txt02.jpg"} vocabId={2} />
-      <Card src={"/img/txt03.jpg"} vocabId={3} />
-      <Card src={"/img/txt04.jpg"} vocabId={4} />
-      <Card src={"/img/txt05.jpg"} vocabId={5} />
-      <Card src={"/img/txt06.jpg"} vocabId={6} />
-    </div>
-  )
+export default class DisplayGrid extends Component {
+  
+  render() {
+    return (
+      <div id="display">
+        <div id="image-grid">
+          <Card src={"/img/img01.jpg"} vocabId={1} />
+          <Card src={"/img/img02.jpg"} vocabId={2} />
+          <Card src={"/img/img03.jpg"} vocabId={3} />
+          <Card src={"/img/img04.jpg"} vocabId={4} />
+          <Card src={"/img/img05.jpg"} vocabId={5} />
+          <Card src={"/img/img06.jpg"} vocabId={6} />
+          <Card src={"/img/txt01.jpg"} vocabId={1} />
+          <Card src={"/img/txt02.jpg"} vocabId={2} />
+          <Card src={"/img/txt03.jpg"} vocabId={3} />
+          <Card src={"/img/txt04.jpg"} vocabId={4} />
+          <Card src={"/img/txt05.jpg"} vocabId={5} />
+          <Card src={"/img/txt06.jpg"} vocabId={6} />
+        </div>
+      </div>
+    );
+  }
+  
 }
