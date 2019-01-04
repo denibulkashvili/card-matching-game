2c24d7366ae3858fe9b48693259298cf201a46ca
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 16:42:34 +0700

    Mix the cards

--- card-matching-game/src/components/DisplayGrid.jsx
+++ card-matching-game/src/components/DisplayGrid.jsx
@@ -8,16 +8,16 @@ export default class DisplayGrid extends Component {
         <div id="image-grid">
           <Card src={"/img/img01.JPG"} vocabId={1} />
           <Card src={"/img/img02.JPG"} vocabId={2} />
-          <Card src={"/img/img03.JPG"} vocabId={3} />
-          <Card src={"/img/img04.JPG"} vocabId={4} />
+          <Card src={"/img/txt03.JPG"} vocabId={3} />
           <Card src={"/img/img05.JPG"} vocabId={5} />
+          <Card src={"/img/txt04.JPG"} vocabId={4} />
+          <Card src={"/img/txt06.JPG"} vocabId={6} />
+          <Card src={"/img/img04.JPG"} vocabId={4} />
           <Card src={"/img/img06.JPG"} vocabId={6} />
           <Card src={"/img/txt01.JPG"} vocabId={1} />
           <Card src={"/img/txt02.JPG"} vocabId={2} />
-          <Card src={"/img/txt03.JPG"} vocabId={3} />
-          <Card src={"/img/txt04.JPG"} vocabId={4} />
+          <Card src={"/img/img03.JPG"} vocabId={3} />
           <Card src={"/img/txt05.JPG"} vocabId={5} />
-          <Card src={"/img/txt06.JPG"} vocabId={6} />
         </div>
       </div>
     );
