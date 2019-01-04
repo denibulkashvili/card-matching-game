dc81536e434af8c0d6900e613f9990141fcd52ca
denibulkashvili <denibulkashvili@gmail.com>
2019-01-04 16:16:41 +0700

    Fix img extension caps bug

--- card-matching-game/src/components/DisplayGrid.jsx
+++ card-matching-game/src/components/DisplayGrid.jsx
@@ -6,18 +6,18 @@ export default class DisplayGrid extends Component {
     return (
       <div id="display">
         <div id="image-grid">
-          <Card src={"/img/img01.jpg"} vocabId={1} />
-          <Card src={"/img/img02.jpg"} vocabId={2} />
-          <Card src={"/img/img03.jpg"} vocabId={3} />
-          <Card src={"/img/img04.jpg"} vocabId={4} />
-          <Card src={"/img/img05.jpg"} vocabId={5} />
-          <Card src={"/img/img06.jpg"} vocabId={6} />
-          <Card src={"/img/txt01.jpg"} vocabId={1} />
-          <Card src={"/img/txt02.jpg"} vocabId={2} />
-          <Card src={"/img/txt03.jpg"} vocabId={3} />
-          <Card src={"/img/txt04.jpg"} vocabId={4} />
-          <Card src={"/img/txt05.jpg"} vocabId={5} />
-          <Card src={"/img/txt06.jpg"} vocabId={6} />
+          <Card src={"/img/img01.JPG"} vocabId={1} />
+          <Card src={"/img/img02.JPG"} vocabId={2} />
+          <Card src={"/img/img03.JPG"} vocabId={3} />
+          <Card src={"/img/img04.JPG"} vocabId={4} />
+          <Card src={"/img/img05.JPG"} vocabId={5} />
+          <Card src={"/img/img06.JPG"} vocabId={6} />
+          <Card src={"/img/txt01.JPG"} vocabId={1} />
+          <Card src={"/img/txt02.JPG"} vocabId={2} />
+          <Card src={"/img/txt03.JPG"} vocabId={3} />
+          <Card src={"/img/txt04.JPG"} vocabId={4} />
+          <Card src={"/img/txt05.JPG"} vocabId={5} />
+          <Card src={"/img/txt06.JPG"} vocabId={6} />
         </div>
       </div>
     );
